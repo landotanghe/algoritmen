5f11b4598b280bad528d58708b4bf3acbb6e46ed
landotanghe <lando.tanghe@gmail.com>
2014-11-24 18:17:53 +0100

    prutsen

---
+++ shiftAND/shiftand.cpp
@@ -0,0 +1,8 @@
+#include "shiftand.h"
+#include <iostream>
+
+int main(){
+    
+
+    return 0;   
+}
--- shiftAND/shiftand.h
+++ shiftAND/shiftand.h
@@ -1,13 +1,18 @@
 #include "bitpatroon.h"
 #include <queue>
+#include <vector>
+using namespace std;
+
+
 typedef unsigned int uint;
 typedef unsigned char uchar;
+const int max_patroonlengte=sizeof(uint)*8;
 class Shiftand{
 public:
 //naaldlengte moet kleiner dan patroonlengte zijn,
 //anders wordt een exceptie opgegooid.
     Shiftand(const uchar* naald,uint _naaldlengte);
-    void zoek(std::queue<const uchar*>& plaats,
+    void zoek(queue<const uchar*>& plaats,
                      const uchar* hooiberg, uint hooiberglengte);
 private:
     Bitpatroon letterpatroon[256];
@@ -16,11 +21,23 @@ private:
     
 };
 
-Shiftand::Shiftand(const uchar* _naald,uint _naaldlengte):
+Shiftand::Shiftand(const uchar* naald,uint _naaldlengte):
 naaldlengte(_naaldlengte)
 {
- 
-    for(int i=0;i<naaldlengte;i++){
-        naald[i]=_naald[i];
+    //beveilig tegen te groot patroon:
+    if(naaldlengte>max_patroonlengte){
+        naaldlengte = max_patroonlengte;   
     }
+    //tabel S uit cursus= letterpatroon[]
+    for(int i=0;i<naaldlengte;i++){
+        int letterwaarde=(int)_naald[i];
+        letterpatroon[letterwaarde].eenbit(i);
+    }
+}
+
+Shiftand::zoek(queue<const uchar*>& plaats,const uchar* hooiberg, uint hooiberglengte){
+    Bitpatroon suffix_lengte* = new Bitpatroon[hooiberg]; //duidt voor index i aan of de prefix met lengte i-1 uit naald in de tekst matcht met de string die op j eindigt.
+    suffix_geldig
+    
+    delete[] suffix_geldig;
 }
