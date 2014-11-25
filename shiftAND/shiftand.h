16d8be2b4cbfcdabb81e0aa4efb611469b75ed85
landotanghe <lando.tanghe@gmail.com>
2014-11-25 16:21:36 +0100

    werkt

--- shiftAND/shiftand.h
+++ shiftAND/shiftand.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <queue>
 #include <vector>
+#include <string>
 using namespace std;
 
 
@@ -42,7 +43,7 @@ naaldlengte(_naaldlengte)
 void Shiftand::zoek(queue<const uchar*>& plaats,const uchar* hooiberg, uint hooiberglengte){
     Bitpatroon *suffix_lengte_voldoet = new Bitpatroon[hooiberglengte+1]; //i(bit) duidt aan of de prefix met lengte i-1 uit naald in de tekst matcht met de string die op j(index) eindigt.
     Bitpatroon result_finder;
-    result_finder.setBit(naaldlengte);//bv  000001000
+    result_finder.setBit(naaldlengte-1);//bv  000001000
                             //als dan R[j]= 110101000
                             //dan zal en()= 000001000 => true (dus enkel als de p-de bit aanstaat)
     
@@ -50,7 +51,9 @@ void Shiftand::zoek(queue<const uchar*>& plaats,const uchar* hooiberg, uint hooi
     for(int j=0;j<hooiberglengte;j++){
         //R [j+1] = shift( R[j]) && S[t[J+1]]
         suffix_lengte_voldoet[j+1]= suffix_lengte_voldoet[j].shiftrechts(1);
+        suffix_lengte_voldoet[j+1].setBit(0);
         suffix_lengte_voldoet[j+1]&= letterpatroon[hooiberg[j]];
+        cout << "R[" << j+1 <<"]=   \t" << suffix_lengte_voldoet[j+1] << endl;
         if(result_finder.en( suffix_lengte_voldoet[j+1])){
             plaats.push(& hooiberg[j-naaldlengte+1]);
         }
